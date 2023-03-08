495bfd4275e34168355ad837a48a66fae6ad7140
Bashi-7869 <121685218+Bashi-7869@users.noreply.github.com>
2023-03-08 12:16:24 +0530

    question.js

---
+++ question.js
@@ -0,0 +1,44 @@
+var questions = [{
+
+    question: "1. How old is the world?\nA.6.678 billion years\nB.3.758 billion years\nC.8.367 billion years\nD.4.543 billion years\n\n Your Answer: ",
+    Answer: ["D", "4.543 billion years"]
+    },
+   {
+   question: "2. How many countries are in the world?? \nA.195\nB.276\nC.185\nD.175\n\n Your Answer: ",
+    Answer: ["A", "195"]
+  },
+  {
+   question: "3. Why does the Statue of Liberty hold a torch?\nA.It symbolizes Unity\nB.It symbolizes Humanity\nC.It symbolizes Enlightenment\nD.It symbolizes help\n\n Your Answer: ",
+    Answer: ["C", "It symbolizes Enlightenment"]
+  },
+  {
+   question: "4. How many seconds are in a day?\nA.86800 sconds\nB.86400 seconds\nC.784500 sconds\nD.65800 sconds\n\n Your Answer: ",
+    Answer:["B", "86400 seconds"]
+  },
+  {
+   question: "5. What was the first domain name registered?\nA.syminam.com\nB.alpha.com\nC.bitcomia.com.com\nD.symbolics.com\n\n Your Answer: ",
+    Answer: ["D", "symbolics.com"]
+  },
+  {
+   question: "6. Men or Women, who are more forgetful?\nA.Women\nB.Men\nC.Men and Women\nD.None of the above\n\n Your Answer: ",
+    Answer: ["B", "Men"]
+  },
+   {
+   question: "7. Who started first blogging?\nA.Justin Hall\nB.Amit Aggarwal\nC.Gary Pepper\nD.Pew Die Pie\n\n Your Answer: ",
+    Answer: ["A", "Justin Hall"]
+  },
+  {
+   question: "8. What is the color of the blood of insects?\nA.White\nB.Yellow\nC.Green\nD.Red\n\n Your Answer: ",
+    Answer: ["B", "Yellow"]
+  },
+  {
+   question: "9. Which country has provided a faster network like 7G?\nA.Netherlands\nB.Hungary\nC.Norway\nD.South Korea\n\n Your Answer: ",
+    Answer: ["C", "Norway"]
+  },
+    {
+    question: "10. What is the longest river in the world in 2018? \nA.Nile River\nB.Congo River\nC.Yangtze River\nD.Roe River\n\n Your Answer: ",
+    Answer: ["A", "Nile River"]
+   
+  },]
+  
+  module.exports = questions
